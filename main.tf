11b06547e14b0d79f5388b90066ea3e154dcd971
Ojo-williams Daniel <ojowilliamsdaniel@gmail.com>
2023-04-13 22:23:46 +0100

    fixing terraform

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ ITEM
 # ------------------- lambda function --------------- #
 # Using the assume role policy from above, create a role for lambda
 resource "aws_iam_role" "role_for_lambda" {
-  name               = "iam_for_lambda"
+  name               = "role_for_lambda"
   assume_role_policy = <<EOF
   {
     "Version": "2012-10-17",
